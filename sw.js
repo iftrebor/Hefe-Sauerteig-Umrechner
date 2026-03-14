0ac6fdf1faf7c19a3b12b0c098fba354b628250d
iftrebor <robert.plietzsch@icloud.com>
2026-03-14 15:38:50 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,2 @@
-// Deaktiviert, um Caching-Effekte sicher auszuschließen.
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
