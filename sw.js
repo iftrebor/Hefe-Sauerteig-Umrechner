a9beb68009ec822ece5bc49d417aa51251ea1ea0
iftrebor <robert.plietzsch@icloud.com>
2026-03-14 15:48:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,17 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(Promise.resolve());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((k) => caches.delete(k)));
+    await self.registration.unregister();
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', () => {
+  // absichtlich leer: keine Offline-Logik, kein Caching
+});
