25ddf6db01a045d3a65de4e3e192d4da6e86d79c
iftrebor <robert.plietzsch@icloud.com>
2026-03-14 17:22:14 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.registration.unregister().then(() => self.clients.matchAll()).then((clients) => {
-    clients.forEach((client) => client.navigate(client.url));
-  }));
+  event.waitUntil(self.clients.claim());
 });
+self.addEventListener('fetch', () => {});
